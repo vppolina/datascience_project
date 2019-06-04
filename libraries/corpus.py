0a1bc9123de4825f506f0674c98b89e674a0493d
luisdiegocm <ldconejo@gmail.com>
2019-06-05 01:00:30 +0200

    Improving and commenting corpus.py, deleting chinese white papers, improve Text Classification nb

--- libraries/corpus.py
+++ libraries/corpus.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import os
 import nltk
 from nltk.corpus import stopwords
@@ -11,19 +9,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import re
 
-###########################################################
-###########################################################
-###########################################################
-
-
-##### TODO
-
-# - Remove URL
-# - Function that receives a text and generate a corpus per sentence / parragraph.
-# - Review Lemmatization, seems not to be working
-########
-
-# a function to POS-tag each word for lemmatization
 
 
 def get_wordnet_pos(word):
@@ -38,26 +23,12 @@ def get_wordnet_pos(word):
 
 
 # mystops is a user defined list of stopwords
-mystops = ['mr', 'said', 'sir', 'upon', 'mrs', 
-                                    'replied', 'man', 'one', 'little', 
-                                    'say', 'dont', 'old', 'gentleman',
-                                    'time', 'two', 'never', 'see', 'door',
-                                    'dear', 'well', 'now', 'will', 'dear',
-                                    'know', 'head', 'come', 'much', 'hand',
-                                    'o', 'inquired', 'room', 'think',
-                                    'way', 'away', 'great', 'good', 'gentleman', 
-                                    'lady', 'long', 'first', 'made', 
-                                    'back', 'another', 'can', 'take', 'must',
-                                    'just', 'ever', 'face', 'nothing', 'without',
-                                    'ever', 'shall', 'took', 'look', 'friend',
-                                    'oh', 'yes', 'many','last', 'might', 'go',
-                                    'may', 'looking', 'rather', 'got', 'place',
-                                    'mind', 'right', 'house', 'three','every', 'day',
-                                    'put', 'thats', 'quite',
-                                    'call', 'could', 'even', 'eye', 'get', 'give', 'let', 'make', 'open',
-           'reply', 'turn', 'would', '•' , '–','\uf0a7', '◦', 'i.']
+mystops = ['•' , '–','\uf0a7', '◦', 'i.','\u200b','∗','！']
             
+special_stops = ['\u200b']
 
+###################################################
+# PRE-PROCESSING FUNCTION
 
 def makeCleanCorpus(dataset,
                     removePunct=True,
@@ -67,85 +38,132 @@ def makeCleanCorpus(dataset,
                     removeStopw=True,
                     lemmatize=False,
                     removeURL=True,
-                    makeSentences=False):
+                    makeSentences=False,
+                    removeChar = False):
     '''
     The makeCleanCorpus function will look for text files in the directory 
-    specified by abspath. Change this to suit you.
+    specified by the dataset. Change this to suit you.
     Input:
-        dataset:     Dictionary with the files and its text
+        dataset:     Dictionary with the files and its text unprocessed
         removePunct: Should punctuation be removed?
         removeNums:  Should numbers be removed?
         lower:       Should words be converted to lower-case?
         removeStopw: Should stop-words be removed?
-                     Apart for the standard English stop-words, the 
-                     variable "mystops" in the code below allows you to add 
+        stops:       Apart for the standard English stop-words, the 
+                     variable "stops" in the code below allows you to add 
                      your own stop-words
         lemmatize:   Should words be lemmatized? The default is "False"
                      Changing this to true will (really) slow this function down!
+        removeURL    Should URLs be removed?
+        makeSentence Instead of returning a whole corpus, it returns a list of processed sentences
+        removeChar   Remove characters from A to Z that are alone
+    Output:
+        Dictionary with {name of document : whole corpus or list of sentences -> processed}
     '''
+    
+    
     ######
+    # Objects that needs to be download so that NLTK works for preprocessing
+    nltk.download("punkt")
+    nltk.download("stopwords")
     nltk.download('averaged_perceptron_tagger')
+    nltk.download('wordnet')
     ######
     
-    
+    # Initialize final dictionary
     clean_files = {}
-        
+    
+    # Iterates through all the dataset
     for filename, text in dataset.items():
         
         print('Cleaning:', filename)
         
-        StopWords = stopwords.words("english") + mystops
-        
+        # Define all the stopwords
+        allstopwords = stopwords.words("english") + mystops + stops
+                
+        # If lemmatize
         if lemmatize:
+            # Prepare the Lemmatizer Engine
             lemmatizer = WordNetLemmatizer()
         
+        # If remove URL
         if removeURL:
             #text = text.replace("\n"," ")
-            text = re.sub(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.\\~#?&\/\/=\s]*)", "", text)
+            # Remove all the URLs with REGEX
+            text = re.sub(r"[h|H]ttps?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-zA-Z]{2,6}\b([-a-zA-Z0-9@:%_\+.\\~#?&\/\/=]*)", "", text)
         
-        # remove punctuation
-        # use the three argumnt form of text.maketrans()
+        # If remove punctuation and no making sentences
         if (removePunct and not makeSentences):
+            # Remove all the punctuations defined in the string library
             text = text.translate(text.maketrans('', '', string.punctuation))
             
-        # remove punctuation except the point (to make sentences)
+        # If remove punctuation except the point (to make sentences)
         if makeSentences:
+            # Remove all punctuations except the point, which is needed to create the sentences
             text = text.translate(text.maketrans('', '', string.punctuation.replace(".","")))
         
-        # remove numbers
+        # If remove numbers
         if removeNums:
+            # Remove all digits
             text = ''.join([x for x in text if not x.isdigit()])
             
-        # convert to lower-case
+        # If convert to lower-case
         if lower:
+            # Convert all the text to lower case
             text = text.lower()
-                       
-        # lemmatize words
+        
+        if removeChar:
+            # Tokenize the whole text
+            word_list = nltk.word_tokenize(text)
+            # Remove tokens that are a single character
+            text = ' '.join([w for w in word_list if (len(w) > 1 or w == '.')])
+        
+        # If lemmatize words
         if lemmatize:
-            word_list = nltk.word_tokenize(text)    
+            # Tokenize the whole text
+            word_list = nltk.word_tokenize(text)
+            # Apply lemmatization to each token in the text
             text = ' '.join([lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in word_list])
                 
-        # remove stopwords
+        # If remove stopwords
         if removeStopw:
-            text = ' '.join([word for word in text.split() if word not in StopWords])
-             
+            # First, replace this special character
+            text = text.replace('\u200b',"")
+            # Remove Chinese characters
+            # text = text.replace(r'[^\x00-\x7F]+', '')
+            # Then, include only the words that are not included in the stopwords
+            text = ' '.join([word for word in text.split() if word not in allstopwords])
+        
+        if True:
+            # Decoding the text to remove non-ascii characters
+            text = text.encode("ascii", "ignore").decode('utf-8')
+
+        # If make sentences
         if makeSentences:
+            # Create tokens on a sentence level
             text = nltk.tokenize.sent_tokenize(text)
+            # Remove all the points used to separate the sentences
             text = [i.replace(".","") for i in text]
+            # Include only sentences with words on it
             text = [x for x in text if x]
 
-        
+        # Add the file to the dictionary, with the code already pre-processed
         clean_files[filename] = text
     
     print ("Done!")
     return(clean_files)
-# end of function makeCleanCorpus
+
 
 def makeDTM(corpus, tfidf=False):
     '''
         This function will make a Document Term Matrix (DTM) from a corpus 
         passed to it. If tfidf is True, then the DTM returned will have 
         TfIdf values for the terms and not the frequencies.
+        Input:
+            corpus:     Dictionary with the files and the pre-processed text
+            tfidf:      If we want to retrieve only the count of tokens, or the TF-IDF matrix
+        Output:
+            Pandas Dataframe with the corpus translated into vectors
     '''
     
     if (not tfidf):
@@ -163,75 +181,17 @@ def makeDTM(corpus, tfidf=False):
                            index=corpus.keys())
         return(dtm)
 
-# end of function makeDTM
-        
-#################################
-
-# make a clean corpus and then return the cleaned up corpus   
-# for a list of sentences
-
-
-################################################################
-################################################################
-
-# this works, but is still experimental
-
-# controlling sparsity
-
-# LOGIC: replace each non-zero frequency in the DTM with a 1
-# Then, the sum of col is the number of times the term occurs in the corpus.
-# The sparsity is this col sum divided by the number of docs 
-
-# percent is a number between 1 and 100
-def controlSparsity(percent):
+def dictionaryToPandas(dictionary):
+    """
+        Function that receives a dictionary and it transform it into a Panda DataFrame
+        Input:
+            Dictionary
+        Output:
+            DataFrame with index as keys and rows as values
     
-    dtm.iloc[:,1]
+    """
+    df = pd.DataFrame(None, columns=["text"],index=dictionary.keys())
+    for key in dictionary.keys():
+        df.loc[key] = [dictionary[key]]
     
-    dtm.iloc[1]  # some values > 1
-    occ_dtm = dtm.copy(deep=True)
-    occ_dtm.iloc[1]
-    occ_dtm[occ_dtm != 0] = 1
-    occ_dtm.iloc[1]  # all non-zero val are 1
-    dtm.iloc[1]  # has not changed - that's what a deep copy does
-    
-    # now use the occurence matrix occ_dtm to calculate sparsesness
-    
-    occ_col_sums = occ_dtm.sum()  # the no of docs in which each term appears
-    
-    occ_col_sums[occ_col_sums==5]
-    
-    # calculate the sparsity
-    num_docs = len(dtm.index)
-    num_docs
-    sparsity = occ_col_sums/num_docs
-    sparsity
-    len(sparsity)
-    
-    # got the sparsity
-    # now remove terms with sparsity less than specified level
-    sparsity_cutoff = percent/100
-    
-    
-    sp = sparsity[sparsity>sparsity_cutoff]
-    len(sp)
-    sp
-    
-    
-    dtm.index
-    dtm.columns
-    
-    sp.index
-    
-    # for sparsity > sparsity_cutoff, keep these indices:
-    keep_indices = dtm.columns.difference(dtm.columns.difference(sp.index))
-    keep_indices
-    
-    # and here is the sparse DTM!
-    dtm.sparse95 = dtm.loc[:, keep_indices]
-    # the above throws a warning (?) the first time you run it, but it works :)
-    
-    
-    return(dtm.sparse95)
-
-
-#############################################
+    return df   
